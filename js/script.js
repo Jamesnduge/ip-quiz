547d875e0dbd956cef0c4da3e85000f8f17dc324
James Mwangi <james_mwangi@aol.com>
2018-12-09 16:18:43 +0300

    relink jquery file to index page

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-/*  ====================Business logic=====================*/
+      //==== Business logic====//
 
 $(document).ready(function() {
 
-  $("form#form1").submit(function(event){
+  $("form#formOne").submit(function(event){
   var qN1 = $("input:radio[name=language]:checked").val();
   var qN2 = $("input:radio[name=block]:checked").val();
-  var qN3 = $("input:radio[name=jQuery]:checked").val();
+  var qN3 = $("input:radio[name=jquery]:checked").val();
   var qN4 = $("input:radio[name=inline]:checked").val();
   var qN5 = $("input:radio[name=github]:checked").val();
-  var qN6 = $("input:radio[name=dType]:checked").val();
+  var qN6 = $("input:radio[name=type]:checked").val();
 
   var youScore = parseInt(qN1)+parseInt(qN2)+parseInt(qN3)+parseInt(qN4)+parseInt(qN5)+parseInt(qN6);
-  $("#youScore").text("HI THERE! HERE'S WHAT YOU SCORED: " +youScore + "%");
+  $("#youScore").text("HI THERE! HERE'S WHAT YOU SCORED: " +youScore + "out of a possible 96 marks");
 
-      //==== user Interface ====//
+      //==== User Interface ====//
 
-
-  $("form#form1").hide();
-  $("#youScore").show();
+  $("form#formOne").slideUp(1000);
+  $("#youScore").show(1000);
     event.preventDefault();
    });
 
