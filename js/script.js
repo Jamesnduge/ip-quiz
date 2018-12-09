3058b79cdc0d7a11c50f7dfbf41bf64d323f70f4
James Mwangi <james_mwangi@aol.com>
2018-12-09 13:07:00 +0300

    add styles to the button element

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 
-  $("form#fwaweru").submit(function(event){
+  $("form#form1").submit(function(event){
   var qN1 = $("input:radio[name=language]:checked").val();
   var qN2 = $("input:radio[name=block]:checked").val();
   var qN3 = $("input:radio[name=jQuery]:checked").val();
@@ -11,8 +11,14 @@ $(document).ready(function() {
   var qN6 = $("input:radio[name=dType]:checked").val();
 
   var youScore = parseInt(qN1)+parseInt(qN2)+parseInt(qN3)+parseInt(qN4)+parseInt(qN5)+parseInt(qN6);
-  if (youScore > 69) {
-      $("#youScore").text("Congratulations, you scored: " +youScore + "%");
-  } else {
-      $("#youScore").text("Well, your score was: " +youScore + "%. Better luck next time!");
-  }
+  $("#youScore").text("HI THERE! HERE'S WHAT YOU SCORED: " +youScore + "%");
+
+      //==== user Interface ====//
+
+
+  $("form#form1").hide();
+  $("#youScore").show();
+    event.preventDefault();
+   });
+
+});
